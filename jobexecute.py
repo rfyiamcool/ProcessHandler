c3f7c607a5143b8d0c8b3a330d387c6870b0fd8c
芮峰云 <rfyiamcool@163.com>
2015-10-25 23:13:14 +0800

    fix logger not found bug

--- jobexecute.py
+++ jobexecute.py
@@ -7,15 +7,14 @@ import traceback
 from ProcessHandler.lib.log import setup_file_logging
 from ProcessHandler.lib.workers.sync import SyncWorker
 
-logger = setup_file_logging('jobexecute','master.log')
 class JobExecute(SyncWorker):
 
     LOGGER_NAME = "jobexecute"
 
     def __init__(self, cfg, file_logger=None, ppid=None, sockets=None):
         SyncWorker.__init__(self, cfg, file_logger, ppid)
-#        setup_file_logging(self.LOGGER_NAME, self.cfg.log_file)
-#        self.logger = logging.getLogger(self.LOGGER_NAME)
+        setup_file_logging(self.LOGGER_NAME, self.cfg.log_file)
+        self.logger = logging.getLogger(self.LOGGER_NAME)
 
     def setup(self):
         super(JobExecute, self).setup()
@@ -29,7 +28,7 @@ class JobExecute(SyncWorker):
     def handle_request(self):
         while 1:
             print 'go....'
-            logger.info('go...')
+            self.logger.info('go...')
             time.sleep(1.5)
 
 if __name__ == '__main__':
